d1529fe038e4169c3bdd8cb1a374b870050b804e
= <anhdt@demetio.com>
2023-12-19 09:55:35 +0700

    Add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
       borderColor: {
         main: 'rgba(37,40,47,0.1)',
       },
+      textColor: {
+        primary: 'rgb(34, 204, 136)',
+        'black-500': 'rgba(37,40,47,1)',
+        'gray-500': 'rgba(157,158,163,1)',
+        'icon-gray': '#7a7b80',
+        'icon-light-gray': '#c4c5c6',
+      },
+      fontSize: {
+        sm: '11px',
+        md: '14px',
+        lg: '20px',
+      },
+      fontFamily: {
+        normal: 'Figtree',
+      },
+      backgroundColor: {
+        main: '#fff',
+      },
+
       screens: {
         desktop: '900px',
       },
