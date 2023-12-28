da16a8691c7eb0ef06444d79547d65808aeae514
anhdt <anhdt@demetio.com>
2023-12-28 14:17:49 +0700

    Separating manually setting size component

--- src/app/molecules/BackgroundSizePopover.tsx
+++ src/app/molecules/BackgroundSizePopover.tsx
@@ -6,17 +6,15 @@ import {
   PopoverTrigger,
 } from '@nextui-org/react';
 import clsx from 'clsx';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronDown, Ruler } from '../icons';
 import { convertFrameSize, getDisplayDimension } from '../utilities/units';
 import { usePageSize } from '../store/use-page-size';
-import { PageSizeGroupInput } from './PageSizeGroupInput';
-import * as Yup from 'yup';
-import { ValidationError } from 'yup';
-import { PageSize, PageSizeUnits } from '../types';
+import { PageSize } from '../types';
 import { ListRecommendedSizes } from './ListRecommendedSizes';
 import { getRecommendedSize } from '../services/page.service';
 import { UNITS } from '../constants/unit-constants';
+import { ManuallySettingSize } from './ManuallySettingSize';
 
 interface BackgroundSizePopoverProps {}
 
@@ -26,98 +24,6 @@ export const BackgroundSizePopover: React.FC<
   const pageSize = usePageSize();
 
   const [sizes, setSizes] = useState<PageSize[]>([]);
-
-  const [pageSizeForm, setPageSizeForm] = useState<{
-    workingWidth: number;
-    workingHeight: number;
-    workingUnit: PageSizeUnits;
-    cuttingWidth: number;
-    cuttingHeight: number;
-    cuttingUnit: PageSizeUnits;
-  }>({
-    workingHeight: pageSize.workingHeightPixels,
-    workingWidth: pageSize.workingHeightPixels,
-    workingUnit: pageSize.workingUnit,
-    cuttingHeight: pageSize.cuttingHeightPixels,
-    cuttingWidth: pageSize.cuttingWidthPixels,
-    cuttingUnit: pageSize.cuttingUnit,
-  });
-
-  const [errors, setErrors] = useState<{ [k: string]: string }>({});
-
-  const handleSubmit = useCallback(
-    async (e: React.FormEvent) => {
-      e.preventDefault();
-      try {
-        const maxNumber = convertFrameSize(
-          'px',
-          pageSizeForm.workingUnit,
-          8000,
-          2,
-        );
-        const minNumber = convertFrameSize(
-          'px',
-          pageSizeForm.workingUnit,
-          1,
-          2,
-        );
-
-        const minMessage = 'Greater than ' + minNumber;
-        const maxMessage = 'Smaller than ' + maxNumber;
-        const schema = Yup.object({
-          workingWidth: Yup.number()
-            .required('required!')
-            .min(minNumber, minMessage)
-            .max(maxNumber, maxMessage),
-          workingHeight: Yup.number()
-            .required('required!')
-            .min(minNumber, minMessage)
-            .max(maxNumber, maxMessage),
-          cuttingWidth: Yup.number()
-            .required('required!')
-            .min(minNumber, minMessage)
-            .max(maxNumber, maxMessage),
-          cuttingHeight: Yup.number()
-            .required('required!')
-            .min(minNumber, minMessage)
-            .max(maxNumber, maxMessage),
-        });
-
-        const getConvertedNumber = (number: number) => {
-          return convertFrameSize('px', pageSizeForm.workingUnit, number, 2);
-        };
-
-        await schema.validate(
-          {
-            workingWidth: getConvertedNumber(pageSizeForm.workingWidth),
-            workingHeight: getConvertedNumber(pageSizeForm.workingHeight),
-            cuttingWidth: getConvertedNumber(pageSizeForm.cuttingWidth),
-            cuttingHeight: getConvertedNumber(pageSizeForm.cuttingHeight),
-          },
-          {
-            abortEarly: false,
-          },
-        );
-
-        setErrors({});
-
-        pageSize.update(pageSizeForm);
-      } catch (e) {
-        if (e instanceof ValidationError) {
-          const tempErrors: { [k: string]: string } = {};
-          e.inner.forEach(error => {
-            if (error.path && error.message) {
-              tempErrors[error.path] = error.message;
-            }
-          });
-
-          console.debug(tempErrors);
-          setErrors(tempErrors);
-        }
-      }
-    },
-    [pageSize, pageSizeForm],
-  );
 
   const onSelectRecommendedSize = (size: PageSize) => {
     const width = convertFrameSize(size.unit, UNITS.PIXEL, size.width, null);
@@ -192,76 +98,7 @@ export const BackgroundSizePopover: React.FC<
                 </div>
               }
             >
-              <div className="w-full px-9 ">
-                <form onSubmit={handleSubmit}>
-                  <div className="grid grid-cols-1 gap-1">
-                    <PageSizeGroupInput
-                      label="Cutting Line"
-                      changeHeight={workingHeightPixels => {
-                        console.debug(workingHeightPixels);
-                        // pageSize.update({ workingHeightPixels });
-                        setPageSizeForm(s => ({
-                          ...s,
-                          workingHeight: workingHeightPixels,
-                        }));
-                      }}
-                      changeWidth={workingWidthPixels => {
-                        // pageSize.update({ workingWidthPixels });
-                        setPageSizeForm(s => ({
-                          ...s,
-                          workingWidth: workingWidthPixels,
-                        }));
-                      }}
-                      changeUnit={workingUnit =>
-                        setPageSizeForm(s => ({
-                          ...s,
-                          workingUnit: workingUnit,
-                        }))
-                      }
-                      height={pageSize.workingHeightPixels}
-                      width={pageSize.workingWidthPixels}
-                      unit={pageSize.workingUnit}
-                      errors={{
-                        width: errors.workingWidth,
-                        height: errors.workingHeight,
-                      }}
-                    />
-                    <PageSizeGroupInput
-                      label="Printing Line"
-                      changeHeight={cuttingHeightPixels => {
-                        // pageSize.update({ cuttingHeightPixels });
-                        setPageSizeForm(s => ({
-                          ...s,
-                          cuttingHeight: cuttingHeightPixels,
-                        }));
-                      }}
-                      changeWidth={cuttingWidthPixels => {
-                        setPageSizeForm(s => ({
-                          ...s,
-                          cuttingWidth: cuttingWidthPixels,
-                        }));
-                      }}
-                      changeUnit={cuttingUnit => {
-                        setPageSizeForm(s => ({
-                          ...s,
-                          cuttingUnit,
-                        }));
-                      }}
-                      height={pageSize.cuttingHeightPixels}
-                      width={pageSize.cuttingWidthPixels}
-                      unit={pageSize.cuttingUnit}
-                      errors={{
-                        width: errors.cuttingWidth,
-                        height: errors.cuttingHeight,
-                      }}
-                    />
-                  </div>
-
-                  <button className="w-full text-md font-normal text-primaryContrast bg-green-500 h-[40px] rounded-sm mt-4">
-                    Apply
-                  </button>
-                </form>
-              </div>
+              <ManuallySettingSize />
             </AccordionItem>
 
             <AccordionItem
