c62f1d023cbe903eda614e9551a71a802f5f71e5
minhnb <minhnb@demetio.com>
2023-12-30 11:31:36 +0700

    Handle show all templates

--- src/app/services/template/template.service.ts
+++ src/app/services/template/template.service.ts
@@ -1,6 +1,45 @@
+import { TemplateFilterType } from '../../types';
 import { Template } from './types';
 import { TemplateCategory } from './types/category';
 
+export const getFilterOptions = async (): Promise<TemplateFilterType[]> => {
+  const templates: TemplateFilterType[] = [
+    {
+      id: 1,
+      type: 'color',
+      colors: [
+        'rgb(255, 82, 82)',
+        'rgb(255, 158, 72)',
+        'rgb(254, 230, 13)',
+        'rgb(169, 234, 46)',
+        'rgb(13, 200, 77)',
+        'rgb(147, 242, 221)',
+        'rgb(56, 128, 255)',
+        'rgb(47, 65, 249)',
+        'rgb(153, 94, 255)',
+        'rgb(255, 132, 234)',
+        'rgb(255, 255, 255)',
+        'rgb(191, 191, 191)',
+        'rgb(0, 0, 0)',
+      ],
+    },
+    {
+      id: 2,
+      type: 'price',
+      prices: [
+        { label: 'Cheap', id: 1 },
+        { label: 'Normal', id: 2 },
+        { label: 'High-end', id: 3 },
+      ],
+    },
+  ];
+
+  return new Promise(res =>
+    setTimeout(() => {
+      res(templates);
+    }, 1000),
+  );
+};
 const mockCategories: TemplateCategory[] = [
   {
     title: 'Banners',
@@ -70,7 +109,6 @@ const mockCategories: TemplateCategory[] = [
     ],
   },
 ];
-
 const mockTemplates: Template[] = [
   {
     thumbnail:
